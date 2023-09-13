cd750915c70619db870c86f84443f1855e4a80d7
madrasmonkey05 <madrascoder05@gmail.com>
2023-09-14 00:58:09 +0530

    day4

---
+++ PatternProblems/pattern14.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        int sum = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                cout << sum << " ";
+                sum += 1;
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/* o/p
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+
+*/
