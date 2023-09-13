3e55e8130c69d7bc87b5084bc3acb23634eadf8a
madrasmonkey05 <madrascoder05@gmail.com>
2023-09-14 01:42:10 +0530

    day4

---
+++ PatternProblems/pattern16.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        for(int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for(int j = n; j >= i; j--) cout << ch++;
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* o/p
+
+ABCDE
+ABCD
+ABC
+AB
+A
+
+*/
