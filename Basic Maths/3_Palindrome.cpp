a17cd00ea7fb56cdf7a29e7482cfec4dda51a530
madrasmonkey05 <madrascoder05@gmail.com>
2023-09-18 22:36:10 +0530

    Day8

---
+++ Basic Maths/3_Palindrome.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    int temp = n, ans = 0;
+		    while(n != 0) {
+		        ans = ans * 10 + (n % 10);
+		        n /= 10;
+		    }
+		    if(ans == temp) return "Yes";
+		    else return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
