3aa6893a657ebedf87f4f21d6f500aa0db2add39
madrasmonkey05 <madrascoder05@gmail.com>
2023-10-01 20:42:49 +0530

    Day4 BasicRecursion Completed

--- Basic Recursion/8_Fibonacci.cpp
+++ Basic Recursion/8_Fibonacci.cpp
@@ -22,30 +22,54 @@ int main()
     return 0;
 }
 
-//Approach 2.0
+// //Approach 2.0
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
+// int main()
+// {
+//     int n;
+//     cin >> n;
 
-    int a = 0, b = 1;
+//     int a = 0, b = 1;
     
-    if(n == 0) cout << a;
-    else if(n == 1) cout << a << " " << b << " ";
-    else {
-        int sum;
-        cout << a << " " << b << " ";
-        for(int i = 2; i <= n; i++) {
-            sum = a + b;
-            a = b;
-            b = sum;
-            cout << sum << " ";
-        }
-    }
+//     if(n == 0) cout << a;
+//     else if(n == 1) cout << a << " " << b << " ";
+//     else {
+//         int sum;
+//         cout << a << " " << b << " ";
+//         for(int i = 2; i <= n; i++) {
+//             sum = a + b;
+//             a = b;
+//             b = sum;
+//             cout << sum << " ";
+//         }
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
+
+// Recursive Approach
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int rec(int n) {
+//     if(n <= 1) return n;
+//     return rec(n-1) + rec(n-2);
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     if(n == 0) cout << 0;
+//     else if(n == 1) cout << 1;
+//     else {
+//         cout << rec(n);
+//     }
+    
+//     return 0;
+// }
---
+++ Basic Recursion/9_Print_first_n_FibonacciNumbers.cpp
@@ -0,0 +1,53 @@
+//Print first n Fibonacci Numbers - gfg
+
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n) 
+    {
+        vector<long long> ans;
+        long long op, a = 1, b = 0;
+        for(long long i = 1; i <= n; i++) {
+            op = a + b;
+            a = b;
+            b = op;
+            ans.push_back(op);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
